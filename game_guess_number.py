f305384003c8c1b540a5bc7470243e4f9c99926e
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-08 17:02:09 +0300

    Initial commit

--- game_guess_number.py
+++ game_guess_number.py
@@ -1,22 +1,33 @@
 # Игра угадай число
 from random import randint
+
 print('Привет! Как тебя зовут?')
 name = input()
-print('Чтож, ' + name + ', давай начнем, я загадал число, попробуй угадать')
-number = randint(1, 20)
 
-for i in range(6):
-    print('Попробуй угадать: ')
-    guess_number = int(input())
+def guess_number():
 
-    if guess_number > number:
-        print('Твое число слишком большое')
-    if guess_number < number:
-        print('Твое число слишком маленькое')
+    print('Чтож, ' + name + ', давай начнем, я загадал число, попробуй угадать')
+    number = randint(1, 20)
+
+    for i in range(6):
+        print('Попробуй угадать: ')
+        guess_number = int(input())
+
+        if guess_number > number:
+            print('Твое число слишком большое')
+        if guess_number < number:
+            print('Твое число слишком маленькое')
+        if guess_number == number:
+            break
+
     if guess_number == number:
-        break
+        print('Ты угадал за ' + str(i+1) + ' попыток')
+    else:
+        print('Я загадал число ' + str(number))
 
-if guess_number == number:
-    print('Ты угадал за ' + str(i+1) + ' попыток')
-else:
-    print('Я загадал число ' + str(number))
+play_again = 'да'
+while play_again == 'да':
+    guess_number()
+    print('сыграем ещё? Да или нет')
+    play_again = input('')
+    play_again = play_again.lower()
