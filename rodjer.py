ede6dea9709729999b877da9c67b21e323935b05
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-22 16:59:25 +0300

    Initial commit

--- rodjer.py
+++ rodjer.py
@@ -8,6 +8,26 @@ while answer not in {'да', 'нет'}:
     print('Должно быть да или нет.')
     answer = input()
 if answer == 'да':
-    print(name + ', сколько примеров ты готов решить')
+
+    question_quanitity = ''
+    while not question_quanitity.isdigit():
+        print(name + ', сколько примеров ты готов решить')
+        question_quanitity = input()
+
+        if question_quanitity.isdigit():
+            while int(question_quanitity) < 1:
+                print('Должно быть больше 0')
+                question_quanitity = input()
+                while not question_quanitity.isdigit():
+                    print('Должна быть цифра')
+                    question_quanitity = input()
+
+        else:
+            print('Должна быть цифра')
+print('')
+
+
+
+
 else:
     print('''Передумал? Хорошо, может как-нибудь в следующий раз..''')
