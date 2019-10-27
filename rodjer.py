e248e7366c133592261bc382392bde78f694bda9
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-27 13:01:37 +0300

    Initial commit

--- rodjer.py
+++ rodjer.py
@@ -1,3 +1,5 @@
+from time import sleep
+from random import randint, choice
 print('Привет! Меня зовут Роджер. А как тебя?')
 name = input()
 print('приятно познакомиться, ' + name)
@@ -9,7 +11,10 @@ while answer not in {'да', 'нет'}:
     answer = input()
 if answer == 'да':
 
-    question_quanitity = ''
+    question_quanitity = ''  # колличество вопросов
+    count_to = ''  # до скольки будем считать
+
+
     while not question_quanitity.isdigit():
         print(name + ', сколько примеров ты готов решить')
         question_quanitity = input()
@@ -21,10 +26,46 @@ if answer == 'да':
                 while not question_quanitity.isdigit():
                     print('Должна быть цифра')
                     question_quanitity = input()
-
         else:
             print('Должна быть цифра')
-print('')
+
+    while not count_to.isdigit():
+        print(name + ', До скольки будем считать?')
+        count_to = input()
+
+        if count_to.isdigit():
+            while int(count_to) < 1:
+                print('Должно быть больше 0')
+                count_to = input()
+                while not count_to.isdigit():
+                    print('Должна быть цифра')
+                    count_to = input()
+        else:
+            print('Должна быть цифра')
+
+    print('Хорошо, тогда начинаем...')
+    sleep(1)
+    for question in  range (int (question_quanitity)):
+        print(f'Пример {question+1}:')
+        number1 = randint(1, int(count_to))
+        number2 = randint(1, int(count_to))
+        sign = choice('+-')
+        if sign =='+':
+
+        while number1<number2:
+            number1 = randint(1, int(count_to))
+            number2 = randint(1, int(count_to))
+            while number1 + number2>count_to:
+                number1 = randint(1, int(count_to))
+                number2 = randint(1, int(count_to))
+
+
+
+        print(number1, sign, number2)
+
+
+
+
 
 
 
